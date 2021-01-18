a75a01ca18396776e4fe95e9d2f3b4cf76db6434
Swarup Hegde <swaruphegde@gmail.com>
2021-01-19 00:05:43 +0530

    feat: coming soon page

---
+++ src/index.js
@@ -0,0 +1,72 @@
+import { render } from 'react-dom'
+import React, { Suspense } from 'react'
+import { Canvas } from 'react-three-fiber'
+import { useProgress, Html } from '@react-three/drei'
+import { HashRouter as Router, Switch, Route, NavLink, Redirect } from 'react-router-dom'
+
+import Scene1 from './Scene1'
+import Scene2 from './Scene2'
+import Scene3 from './Scene3'
+
+import "./base.css"
+
+function Loader() {
+  const { progress } = useProgress()
+  return (
+    <Html center>
+      <span style={{ color: '#FFFFFF' }}>{progress} % loaded</span>
+    </Html>
+  )
+}
+
+function App(props) {
+  const { scene = 1 } = props
+  return (
+    <Canvas concurrent shadowMap camera={{ position: [0, 0, 5], fov: 70 }}>
+      <color attach="background" args={['#000']} />
+      <Suspense fallback={<Loader />}>
+        {scene === 1 && <Scene1 />}
+        {scene === 2 && <Scene2 />}
+        {scene === 3 && <Scene3 />}
+      </Suspense>
+      <ambientLight intensity={0.4} />
+    </Canvas>
+  )
+}
+
+function Body() {
+  return (
+    <Router>
+      <main>
+        <div className="frame">
+          <div className="frame__title-wrap">
+            <h1 className="frame__title">Silver Fest 3.0</h1>
+            <p className="frame__tagline">stay tuned for further updates</p>
+          </div>
+          <div className="frame__demos">
+            <a href="https://instagram.com/swarup_hegde">about the dev</a>
+            <a href="https://github.com/indraasura">github</a>
+          </div>
+        </div>
+        <div className="content">
+          <Switch>
+            <Route exact path="/">
+              <Redirect to="/panna" />
+            </Route>
+            <Route exact path="/panna">
+              <App scene={1} />
+            </Route>
+            <Route exact path="/olga">
+              <App scene={2} />
+            </Route>
+            <Route exact path="/pedro">
+              <App scene={3} />
+            </Route>
+          </Switch>
+        </div>
+      </main>
+    </Router>
+  )
+}
+
+render(<Body />, document.querySelector('#root'))
